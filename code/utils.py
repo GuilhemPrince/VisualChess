3fbe8a6968dcf2a6f1aad8512197fc71db6eb1ce
Mathian <mathian.akanati@student-cs.fr>
2023-03-28 15:47:24 +0200

    array modeliasation

--- code/utils.py
+++ code/utils.py
@@ -12,12 +12,14 @@ def show_and_get_gray_img(path):
     img_gray = cv2.imread(path,0)
     plt.imshow(img_gray, cmap='gray')
     plt.show()
- 
+    return img_gray
+
 def show_and_get_colored_img(path):
     img_bgr = cv2.imread(path,1)
     img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
     plt.imshow(img_rgb)
     plt.show()
+    return img_rgb
 
 if __name__ == '__main__':
     show_and_get_colored_img('.././photos_test/square/with_pieces/black_on_white_g.png')
